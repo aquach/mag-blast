07a3fcef9dce7b10c7161d90e2c5e252f63b9a75
Alex Quach <alexhquach@gmail.com>
2021-08-15 19:36:12 -0700

    Add README.

---
+++ src/client/README.md
@@ -0,0 +1,3 @@
+# mag-blast
+
+An online clone of the game Mag Blast by Fantasy Flight Games. Play at https://alexqua.ch/mag-blast.
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -17,6 +17,14 @@ const App: React.FunctionComponent = () => {
     <div className="flex flex-column vh-100 w-100 justify-center items-center">
       <h1>Mag Blast</h1>
       <button onClick={onClick}>Create Game</button>
+      <h4>
+        <a href="https://images-cdn.fantasyflightgames.com/filer_public/6b/8a/6b8a9492-f27c-423b-b568-2032d4c3c300/mb_rules.pdf">
+          Rules
+        </a>
+      </h4>
+      <h4>
+
+      </h4>
     </div>
   )
 }
